d0619a528aa1ba5bb2c8ae73c95e83d830f7c06d
Miguel Panelo <miguelrpanelo@gmail.com>
2017-10-08 02:30:11 +0800

    Added Models

---
+++ data/src/main/kotlin/io/github/gumil/data/model/Subreddit.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 Miguel Panelo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.gumil.data.model
+
+import com.squareup.moshi.Json
+import io.github.gumil.data.model.base.Listing
+
+data class Subreddit(
+
+        @Json(name = "before")
+        override val before: String?,
+
+        @Json(name = "after")
+        override val after: String?,
+
+        @Json(name = "modhash")
+        override val modHash: String?,
+
+        @Json(name = "children")
+        override val children: List<Thread>,
+
+        @Json(name = "whitelist_status")
+        val whiteListStatus: String = "all_ads"
+
+) : Listing<Thread> {
+
+}
---
+++ data/src/main/kotlin/io/github/gumil/data/model/Thread.kt
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2017 Miguel Panelo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.gumil.data.model
+
+import com.squareup.moshi.Json
+import io.github.gumil.data.model.base.*
+
+data class Thread(
+
+        @Json(name = "id")
+        override val id: String?,
+
+        @Json(name = "name")
+        override val name: String?,
+
+        @Json(name = "kind")
+        override val kind: Kind? = Kind.T3,
+
+        @Json(name = "data")
+        override val data: Unit?,
+
+        @Json(name = "ups")
+        override val ups: Int,
+
+        @Json(name = "downs")
+        override val downs: Int,
+
+        @Json(name = "likes")
+        override val isLiked: Boolean,
+
+        @Json(name = "created")
+        override val created: Long,
+
+        @Json(name = "created_utc")
+        override val createdUtc: Long,
+
+        @Json(name = "domain")
+        val domain: String,
+
+        @Json(name = "subreddit")
+        val subreddit: String,
+
+        @Json(name = "subreddit_id")
+        val subredditId: String,
+
+        @Json(name = "subreddit_name_prefixed")
+        val subredditPrefix: String,
+
+        @Json(name = "selftext_html")
+        val selfTextHtml: String?,
+
+        @Json(name = "selftext")
+        val selfText: String?,
+
+        @Json(name = "link_flair_text")
+        val flair: String?,
+
+        @Json(name = "title")
+        val title: String,
+
+        @Json(name = "score")
+        val score: Int = 0,
+
+        @Json(name = "over_18")
+        val nsfw: Boolean = false,
+
+        @Json(name = "thumbnail")
+        val thumbnail: String,
+
+        @Json(name = "thumbnail_width")
+        val thumbnailWidth: Int?,
+
+        @Json(name = "thumbnail_height")
+        val thumbnailHeight: Int?,
+
+        @Json(name = "gilded")
+        val gilded: Int,
+
+        @Json(name = "stickied")
+        val isSticky: Int,
+
+        @Json(name = "spoiler")
+        val isSpoiler: Boolean = false,
+
+        @Json(name = "permalink")
+        val permalink: String,
+
+        @Json(name = "subreddit_type")
+        val type: String,
+
+        @Json(name = "locked")
+        val isLocked: Boolean = false,
+
+        @Json(name = "hide_score")
+        val hideScore: Boolean = false,
+
+        @Json(name = "url")
+        val url: String,
+
+        @Json(name = "author")
+        val author: String,
+
+        @Json(name = "num_comments")
+        val numComments: Int,
+
+        @Json(name = "is_self")
+        val isSelf: Boolean = false,
+
+        @Json(name = "distinguished")
+        val distinguished: Distinguish? = null
+
+) : Thing<Unit>, Votable, Created
